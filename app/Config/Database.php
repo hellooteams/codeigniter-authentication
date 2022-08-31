711702cf365b225d11d9fb3614117ce52e68a8f5
Dilip <indladileep1@gmail.com>
2022-08-31 22:31:59 +0530

    updated readme

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'bashinvite',
+        'database' => 'userstable',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
